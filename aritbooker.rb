3154b7e1b48449290e4b444f657dd1cfb274af8d
Ari Turetzky <abturet@edipa.(none)>
2010-05-30 18:00:35 -0500

    fixing / method

--- aritbooker.rb
+++ aritbooker.rb
@@ -2,12 +2,12 @@
 
 require 'rubygems'
 require 'sinatra'
-#require 'frankie'
+require 'frankie'
 
 configure do
-#  set :sessions, true
-#  set :environment, :development
-#  load_facebook_config "./config/facebooker.yml", Sinatra::Application.environment
+  set :sessions, true
+  set :environment, :development
+  load_facebook_config "./config/facebooker.yml", Sinatra::Application.environment
 end
 
 before do
@@ -16,5 +16,5 @@ before do
  end
 
 get '/' do
-body  "<h1>hello</h1>"
+body  "<h1>hello #{session['facebook_session'].user.name} and welcome to frankie</h1>"
 end
