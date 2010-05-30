b14d993f86fb363440a05a230131030e5448290b
Ari Turetzky <abturet@edipa.(none)>
2010-05-30 18:05:56 -0500

    fixing / method

--- aritbooker.rb
+++ aritbooker.rb
@@ -11,8 +11,8 @@ configure do
 end
 
 before do
-#  ensure_authenticated_to_facebook
-#  ensure_application_is_installed_by_facebook_user
+  ensure_authenticated_to_facebook
+  ensure_application_is_installed_by_facebook_user
  end
 
 get '/' do
