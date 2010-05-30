9ea3cc7dbe146c097b07fc7036dc40505be6493d
Ari Turetzky <abturet@edipa.(none)>
2010-05-30 13:56:05 -0500

    first commit

---
+++ aritbooker.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'sinatra'
+require 'frankie'
+
+configure do
+  set :sessions, true
+  set :environment, :development
+  load_facebook_config "./config/facebooker.yml", Sinatra::Application.environment
+end
+
+before do
+  ensure_authenticated_to_facebook
+  ensure_application_is_installed_by_facebook_user
+ end
+
+get '/' do
+  body "<h1>hello #{session['facebook_session'].user.name} and welcome to frankie</h1>"
+end
