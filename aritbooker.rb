0bb0eb9d6cc2833a0efea5ddc09503c308254567
Ari Turetzky <abturet@arit93.(none)>
2010-05-31 11:48:42 -0500

    updates

--- aritbooker.rb
+++ aritbooker.rb
@@ -42,7 +42,7 @@ get '/' do
     friends.each do |a_friend|
       status = a_friend.status
       if status_message(status)
-        bstr += "<p>#{a_friend.name} says #{status_message status}</p>"
+        bstr += "<p>#{a_friend.name} says #{status_message(status)}</p>"
       end
       # {status.instance_variable_get(:@message)}
     end
