71e139ff70c8062790419df539887a25189d2eaa
Ari Turetzky <abturet@arit93.(none)>
2010-05-31 11:23:23 -0500

    updates

--- aritbooker.rb
+++ aritbooker.rb
@@ -42,7 +42,9 @@ get '/' do
     friends =  session[:facebook_session].user.friends!(:name, :status)
     friends.each do |a_friend|
       status = a_friend.status
-      bstr += "<p>#{a_friend.name} says #{status_message status}</p>" if status_message status
+      if status_message status
+        bstr += "<p>#{a_friend.name} says #{status_message status}</p>"
+      end
       # {status.instance_variable_get(:@message)}
     end
     bstr
