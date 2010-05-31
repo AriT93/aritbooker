7eee3c7d907e9e7fd4931f0210e937b937e17278
Ari Turetzky <abturet@arit93.(none)>
2010-05-30 19:46:10 -0500

    updates

--- aritbooker.rb
+++ aritbooker.rb
@@ -16,5 +16,6 @@ before do
  end
 
 get '/' do
-body  "<h1>hello #{session[:facebook_session].user.name} and welcome to frankie</h1>"
+body  "<fb:dashboard><fb:header decoration\"add_border\" icon=\"false\">AriBooker</fb:header></fb:dashboard>
+        <h1>hello #{session[:facebook_session].user.name} and welcome to frankie</h1>"
 end
