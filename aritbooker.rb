e7b6a069bf4098a02bf06a9425896f7c1969d2e9
AriT93 <arit93@yahoo.com>
2010-09-19 20:18:26 -0500

    updates at long last

--- aritbooker.rb
+++ aritbooker.rb
@@ -58,8 +58,6 @@ get '/' do
       @user.save
     end
     @oauth_url = MiniFB.oauth_url(@@yaml["app_id"],@@yaml["callback_url"] + "/sessions/create",:scope=>MiniFB.scopes.join(","))
-    @user.atoken = @access_token
-    @user.save
     @fbs = MiniFB::OAuthSession.new(@user.atoken,"en_US")
     haml :index
   end
