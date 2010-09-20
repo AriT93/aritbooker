90dc96d07a5f696220c7fdc47c868b444157cdce
AriT93 <arit93@yahoo.com>
2010-09-19 20:20:29 -0500

    updates at long last

--- aritbooker.rb
+++ aritbooker.rb
@@ -67,10 +67,12 @@ get '/sessions/create' do
   @access_token_hash = MiniFB.oauth_access_token(@@yaml["app_id"],@@yaml["callback_url"] + "/sessions/create",@@yaml["secret_key"], params[:code])
   @access_token = @access_token_hash["access_token"]
   env[:access_token] = @access_token
-  @user = AbUser.first(:email => current_user.email)
-  @user.atoken = @access_token
-  @user.save
-  @access_token.to_s + " : " + @user.atoken + " : " + current_user.email
+  if @user == nil
+    @user = AbUser.first(:email => current_user.email)
+    @user.atoken = @access_token
+    @user.save
+  end
+  haml :index
 end
 
 
