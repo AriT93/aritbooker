a319dabc5c807207401d115fb899f342f08caaae
AriT93 <arit93@yahoo.com>
2010-09-22 14:47:09 -0500

    irked by commenting

--- aritbooker.rb
+++ aritbooker.rb
@@ -104,8 +104,8 @@ end
 post '/comment/:id' do
   @fbs.post(params[:id], :type => "comments", :params =>{ :message => params[:comment]})
   #params[:comment] + " ; " + params[:id]
-  @fbs.get(params[:id])
 #  redirect '/'
+  true
 end
 
 get '/css/style.css' do
