a17b77e508f068e7553b7671311876341b338014
AriT93 <arit93@yahoo.com>
2010-09-22 10:35:03 -0500

    issues with passing to partial  makes me cranky

--- aritbooker.rb
+++ aritbooker.rb
@@ -100,7 +100,8 @@ get '/comment/:id' do
 end
 
 post '/comment/:id' do
-  @fbs.post(params[:id], :type => "comments", :params =>{ :message => params[:comment]}) + " : " +  params[:comment] + " ; " + params[:id]
+  @fbs.post(params[:id], :type => "comments", :params =>{ :message => params[:comment]})
+   params[:comment] + " ; " + params[:id]
 #  redirect '/'
 end
 
