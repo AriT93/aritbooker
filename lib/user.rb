5aebfe3f12620d50ca972184f8323dd70a56396a
AriT93 <arit93@yahoo.com>
2010-09-23 09:07:02 -0500

    why doesn't my name show up?

--- lib/user.rb
+++ lib/user.rb
@@ -5,5 +5,9 @@ class AbUser
   property :email, String
   property :name, String
   property :atoken, String, :length => 200
-  property :atokenhash, String
+  property :atokenhash, String, :length => 200
+
+  def to_s
+    return "#{id} :: #{email} :: #{name}"
+  end
 end
