8a64b828bc01a6216215d345f0090ffcf0c6482c
AriT93 <arit93@yahoo.com>
2011-02-17 10:17:14 -0600

    i can update status?

--- aritbooker.rb
+++ aritbooker.rb
@@ -107,7 +107,7 @@ post '/comment/:id' do
   true
 end
 post '/status' do
-  @fbs.post('me', :type => :feed, :params =>{ :message => params[:status]}b)
+  @fbs.post('me', :type => :feed, :params =>{ :message => params[:status]})
   redirect '/'
 end
 get '/css/style.css' do
